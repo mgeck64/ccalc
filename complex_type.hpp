78701d20630499656f399db473bf52884bd30237
Mark Geck <mgeck64@gmail.com>
2022-01-03 17:49:16 -0800

    Move definitions from complex_type_defs.hpp to here

--- complex_type.hpp
+++ complex_type.hpp
@@ -48,6 +48,27 @@ auto norm_wrapper(const complex_type& z) -> complex_type;
 
 
 
-#include "complex_type_defs.hpp"
+// definitions------------------------------------------------------------------
+
+
+
+namespace calc_val {
+
+namespace helper { // implementation helpers; not meant for public use
+    auto pow_uint_e(const complex_type& z, max_uint_type e) -> complex_type;
+    auto pow_int_e(const complex_type& z, max_int_type e) -> complex_type;
+}
+
+template <typename T>
+inline auto pow(const complex_type& z, T e) -> std::enable_if_t<
+    std::is_integral_v<T> && std::is_unsigned_v<T>, complex_type>
+{return helper::pow_uint_e(z, e);}
+
+template <typename T>
+inline auto pow(const complex_type& z, T e) -> std::enable_if_t<
+    std::is_integral_v<T> && std::is_signed_v<T>, complex_type>
+{return helper::pow_int_e(z, e);}
+
+} // namespace calc_val
 
 #endif // COMPLEX_TYPE_HPP
